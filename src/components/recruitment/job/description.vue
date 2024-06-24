391363fbd3902a36b7c7c4b2fb4326f40f4e8f93
ng-namanh <namanh200313@gmail.com>
2024-06-24 22:26:13 +0700

    feat: job description component

---
+++ src/components/recruitment/job/description.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <div class="flex items-center gap-2 text-2xl">
+      <UIcon :name="icon" />
+      <h2 class="font-medium text-gray-950">
+        <CommonBrandText
+          :prefix="title.prefix"
+          :color="title.color"
+          :label="title.label"
+          size="2xl"
+        />
+      </h2>
+    </div>
+    <ul class="list-disc pl-5 space-y-2 text-gray-950">
+      <li v-for="item in descriptionItems" :key="id">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { JobDescription } from '~/types/recruitment/job';
+
+const { id, title, icon, descriptionItems } = defineProps<JobDescription>();
+</script>
