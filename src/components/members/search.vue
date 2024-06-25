928b21ed73a47c796daca7a5b236b6e57b4f5a41
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-06-25 22:46:41 +0700

    fix: add ref for filters

--- src/components/members/search.vue
+++ src/components/members/search.vue
@@ -4,21 +4,25 @@
     <div class="flex gap-8 flex-wrap justify-center">
       <USelect
         :options="teamOptions"
+        v-model="teamOption"
         size="xl"
         class="rounded-md min-w-[132px]"
       />
       <USelect
         :options="genOptions"
+        v-model="genOption"
         size="xl"
         class="rounded-md min-w-[132px]"
       />
       <USelect
         :options="levelOptions"
+        v-model="levelOption"
         size="xl"
         class="rounded-md min-w-[132px]"
       />
       <USelect
         :options="sortOptions"
+        v-model="sortOption"
         size="xl"
         class="rounded-md min-w-[132px]"
       />
@@ -27,8 +31,13 @@
 </template>
 
 <script setup lang="ts">
-const teamOptions = ['All Teams']
-const genOptions = ['All Gens']
-const levelOptions = ['All Levels']
-const sortOptions = ['Sort By']
+const teamOptions = ref(['All Teams'])
+const genOptions = ref(['All Gens'])
+const levelOptions = ref(['All Levels'])
+const sortOptions = ref(['Sort By'])
+
+const teamOption = ref()
+const genOption = ref()
+const levelOption = ref()
+const sortOption = ref()
 </script>
